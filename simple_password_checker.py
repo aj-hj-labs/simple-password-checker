73df118a79373b334b56c268cd8aef5462c05f49
honney joy manansala <hj13manansala@gmail.com>
2026-04-20 14:05:45 +0000

    stg2 done

--- simple_password_checker.py
+++ simple_password_checker.py
@@ -1,7 +1,32 @@
-InpPassword = input("Input Password: ")
+isUpperLower = False
+isNumber = False
+isCharacter = False
+
+InpPassword = str(input("Input Password: "))
+
 if len(InpPassword) < 6:
     print("weak")
 elif len(InpPassword) >= 6 and len(InpPassword) <= 10:
     print("medium")
-elif len(InpPassword) > 10:
-    print("strong")
+elif len(InpPassword) > 10: 
+    #print("strong")
+    for pwdStr in InpPassword:
+       if pwdStr.isupper() or pwdStr.islower():
+           isUpperLower = True
+           continue
+       elif pwdStr.isdigit():
+           isNumber = True
+           continue
+       elif not pwdStr.isalnum():
+           isCharacter = True
+           continue
+    if isNumber == True and isUpperLower == True and isCharacter == True:
+        print("Password: ", InpPassword)
+    else:
+        if isNumber == False:
+            print("Invalid Password! It must have a number")
+        elif isUpperLower == False:
+            print("Invalid Password! It must have atelast lower or upper case letter")
+        elif isCharacter == False:
+            print("Invalid Password! It must have a special character")
+
